8ea0ecd9971ec473f1ddf189edbaea859a36336d
Zachary Igielman <zacharyi123@gmail.com>
2014-06-14 11:50:52 +0000

    written buzzer scale c

--- examples/buzzer.py
+++ examples/buzzer.py
@@ -2,3 +2,12 @@
 # buzzer.py
 # plays a scale on the buzzer
 # Author : Zachary Igielman
+
+import RPi.GPIO as GPIO, time
+
+buzzer_pin=23
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(buzzer_pin, GPIO.OUT)
+myPWM=GPIO.PWM(buzzer_pin, 
