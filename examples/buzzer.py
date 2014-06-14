cc7700fd088f182ece86cd84f0971be0e0fdffee
ZacharyIgielman <zacharyi123@me.com>
2014-06-14 14:17:51 +0100

    Update buzzer.py

--- examples/buzzer.py
+++ examples/buzzer.py
@@ -10,4 +10,14 @@ buzzer_pin=23
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(buzzer_pin, GPIO.OUT)
-myPWM=GPIO.PWM(buzzer_pin, 
+myPWM=GPIO.PWM(buzzer_pin, 0)
+myPWM.start(50)
+
+scale=[262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 524]
+
+while True:
+  a=0
+  while a < 13:
+    myPWM.changeFrequency(scale[a])
+    time.sleep(0.5)
+    a=a+1
