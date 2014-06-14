504ec0e916f27835c7d61c7527310b83bce87d25
ZacharyIgielman <zacharyi123@me.com>
2014-06-14 12:52:20 +0100

    Update buzzer.c

--- examples/buzzer.c
+++ examples/buzzer.c
@@ -1,27 +1,6 @@
-/*
- * softTone.c:
- *	Test of the softTone module in wiringPi
- *	Plays a scale out on pin 3 - connect pizeo disc to pin 3 & 0v
- *
- * Copyright (c) 2012-2013 Gordon Henderson. <projects@drogon.net>
- ***********************************************************************
- * This file is part of wiringPi:
- *	https://projects.drogon.net/raspberry-pi/wiringpi/
- *
- *    wiringPi is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    wiringPi is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
- */
+//buzzer.c
+//plays a scale on the buzzer
+//Author : Zachary Igielman
 
 #include <stdio.h>
 #include <errno.h>
